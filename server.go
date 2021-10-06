38dfb7d5ea26261fed5fcb7cdb4a75e48dfee873
hai061898 <truonghaiauto98@gmail.com>
2021-10-06 21:24:54 +0700

    #20

--- server.go
+++ server.go
@@ -2,29 +2,53 @@ package main
 
 import (
 	"api1/config"
-    "api1/controllers"
+	"api1/controllers"
+	"api1/middleware"
+	"api1/repository"
+	"api1/service"
 
 	"github.com/gin-gonic/gin"
+
 	"gorm.io/gorm"
 )
 
 var (
-	db             *gorm.DB                  = config.SetupDatabaseConnection() 
+	db             *gorm.DB                  = config.SetupDatabaseConnection()
+	userRepository repository.UserRepository = repository.NewUserRepository(db)
+	bookRepository repository.BookRepository = repository.NewBookRepository(db)
+	jwtService     service.JWTService        = service.NewJWTService()
+	userService    service.UserService       = service.NewUserService(userRepository)
+	bookService    service.BookService       = service.NewBookService(bookRepository)
+	authService    service.AuthService       = service.NewAuthService(userRepository)
 	authController controller.AuthController = controller.NewAuthController(authService, jwtService)
+	userController controller.UserController = controller.NewUserController(userService, jwtService)
+	bookController controller.BookController = controller.NewBookController(bookService, jwtService)
 )
+
 func main() {
 	defer config.CloseDatabaseConnection(db)
 	r := gin.Default()
+
 	authRoutes := r.Group("api/auth")
 	{
 		authRoutes.POST("/login", authController.Login)
-		authRoutes.POST("/login", authController.Register)
+		authRoutes.POST("/register", authController.Register)
 	}
-	// r.GET("/", func(c *gin.Context) {
-	// 	c.JSON(200, gin.H{
-	// 		"message": "Hello",
-	// 	})
-	// })
 
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	userRoutes := r.Group("api/user", middleware.AuthorizeJWT(jwtService))
+	{
+		userRoutes.GET("/profile", userController.Profile)
+		userRoutes.PUT("/profile", userController.Update)
+	}
+
+	bookRoutes := r.Group("api/books", middleware.AuthorizeJWT(jwtService))
+	{
+		bookRoutes.GET("/", bookController.All)
+		bookRoutes.POST("/", bookController.Insert)
+		bookRoutes.GET("/:id", bookController.FindByID)
+		bookRoutes.PUT("/:id", bookController.Update)
+		bookRoutes.DELETE("/:id", bookController.Delete)
+	}
+
+	r.Run()
 }
